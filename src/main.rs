85dad6b80832cea6e379366f6c29696bc9d3d549
Felipe Torres González <torresfelipex1@gmail.com>
2024-07-19 13:49:39 +0200

    Simple JSON response for the endpoint ingredient
    [GH-2]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
+use serde::Serialize;
+
+#[allow(dead_code)]
+#[derive(Serialize)]
+enum Category {
+    Spirit,
+    Bitter,
+    SoftDrink,
+    Garnish,
+    Other,
+}
+#[derive(Serialize)]
+struct Ingredient {
+    name: String,
+    category: Category,
+    desc: String,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -10,19 +27,21 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[get("/ingredient")]
+async fn ingredient() -> Result<impl Responder> {
+    let test_ingredient = Ingredient {
+        name: "Vodka".to_string(),
+        category: Category::Spirit,
+        desc: "Voka liquour 40%".to_string(),
+    };
+
+    Ok(web::Json(test_ingredient))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(hello).service(echo).service(ingredient))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
