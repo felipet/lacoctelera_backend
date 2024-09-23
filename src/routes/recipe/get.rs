1dc2c043c38cd9601d9ee7506e2829826d8e240b
Felipe Torres González <torresfelipex1@gmail.com>
2024-09-23 11:44:52 +0200

    Add license header to all source code files

--- src/routes/recipe/get.rs
+++ src/routes/recipe/get.rs
@@ -1,3 +1,9 @@
+// Copyright 2024 Felipe Torres González
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! Example
 
 use crate::domain::{QuantityUnit, Recipe, RecipeCategory, RecipeContains, RecipeQuery, Tag};
