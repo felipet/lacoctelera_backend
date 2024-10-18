842a33a38896284e83f6fac26f47f246aba20d14
Felipe Torres González <torresfelipex1@gmail.com>
2024-10-18 14:58:30 +0200

    Modified SQL DB Schema for the token management
    [GH-17]

--- migrations/20241017100845_token_request_schema.sql
+++ migrations/20241017100845_token_request_schema.sql
@@ -4,11 +4,12 @@
 
 DROP TABLE IF EXISTS `ApiUser`;
 CREATE TABLE `ApiUser` (
-    `id` INT auto_increment NOT NULL,
+    `id` VARCHAR(36) NOT NULL,
     `name` varchar(40) NULL,
     `email` varchar(80) NOT NULL,
     `validated` BOOL DEFAULT false NULL,
     `enabled` BOOL DEFAULT false NULL,
+    `explanation` TEXT NOT NULL,
     CONSTRAINT `ApiUser_PK` PRIMARY KEY (`id`)
 );
 
@@ -17,7 +18,7 @@ CREATE TABLE `ApiToken` (
     `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `api_token` varchar(25) NOT NULL,
     `valid_until` DATE NOT NULL,
-    `client_id` INT NOT NULL,
+    `client_id` VARCHAR(36) NOT NULL,
     CONSTRAINT `ApiToken_PK` PRIMARY KEY (`api_token`),
     CONSTRAINT `ApiToken_ApiUser_FK` FOREIGN KEY (`client_id`) REFERENCES `ApiUser` (`id`) ON DELETE CASCADE
 );
