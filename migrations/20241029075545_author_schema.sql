30f0434cf102eb123514495cf638df76aab44afa
Felipe Torres González <torresfelipex1@gmail.com>
2024-10-29 09:26:53 +0100

    Add DB migration to include author's objects
    [GH-27]

---
+++ migrations/20241029075545_author_schema.sql
@@ -0,0 +1,37 @@
+-- -------------------------------------
+-- DB Schema for the API author endpoint
+-- -------------------------------------
+
+-- Table that stores all the supported social profiles.
+CREATE TABLE `SocialProfile` (
+    `provider_name` varchar(40) NOT NULL,
+    `website` VARCHAR(80) NOT NULL,
+    CONSTRAINT `SocialProfile_PK` PRIMARY KEY (`provider_name`)
+);
+
+-- Load some Social Media profiles into the DB.
+INSERT INTO `SocialProfile` VALUES
+    ('Facebook', 'https://www.facebook.com/'),
+    ('X', 'https://x.com/'),
+    ('Instagram', 'https://www.instagram.com/');
+
+-- Relation between social profiles and authors.
+CREATE TABLE `AuthorHashSocialProfile` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `provider_name` VARCHAR(40) NOT NULL,
+    `user_name` VARCHAR(40) NOT NULL,
+    CONSTRAINT `SocialProfile_Provider_FK` FOREIGN KEY (`provider_name`)
+        REFERENCES `SocialProfile` (`provider_name`) ON DELETE CASCADE
+);
+
+-- Author table.
+CREATE TABLE `Author` (
+    `id` VARCHAR(40) NOT NULL,
+    `name` VARCHAR(40) NOT NULL,
+    `surname` VARCHAR(40) NOT NULL,
+    `email` VARCHAR(80) NOT NULL,
+    `shareable` BOOLEAN DEFAULT TRUE,
+    `description` VARCHAR(255),
+    `website` VARCHAR(80),
+    CONSTRAINT PRIMARY KEY (`id`)
+);
