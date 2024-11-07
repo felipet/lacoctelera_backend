63f46d07b601ed6cda46a2cc7e31b753aa13e8fe
Felipe Torres González <torresfelipex1@gmail.com>
2024-11-07 13:30:11 +0100

    Minimal refactor of PATH for /author and improved docs
    [GH-24]

--- src/routes/author/patch.rs
+++ src/routes/author/patch.rs
@@ -20,32 +20,33 @@ use sqlx::MySqlPool;
 use std::error::Error;
 use tracing::{debug, info, instrument};
 
-/// PATCH method for the Author endpoint (Restricted).
+/// Resource that allows to modify some of the attributes of an existing author in the DB.
 ///
 /// # Description
 ///
-/// This method updates an [Author] entry in the DB if the given [AuthorId] matches the ID of a
-/// registered author.
+/// This singleton resource of `/author` changes the content for the attributes given in the request body. The
+/// resource accepts a JSON object, that defines (part of) an author entry of the DB. The bare minimum is to include
+/// the author's ID, and an attribute to modify its content.
 ///
-/// This method requires to authenticate the client using a valid [crate::AuthData::api_key].
+/// This resource requires the API client to provide an API token.
 #[utoipa::path(
     patch,
-    path = "/author",
+    context_path = "/author/",
     tag = "Author",
-    request_body(
-        content = Author, description = "A partial definition of an Author entry.",
-        example = json!({"id": "0191e13b-5ab7-78f1-bc06-be503a6c111b", "surname": "Doe"})
-    ),
-    responses(
-        (status = 204, description = "The author entry was updated in the DB."),
-        (status = 401, description = "The client has no access to this resource."),
-        (status = 404, description = "An author identified by the given ID was not existing in the DB."),
-    ),
     security(
         ("api_key" = [])
+    ),
+    request_body(
+        content = Author, description = "A partial definition of an Author entry.",
+        example = json!({"id": "0191e13b-5ab7-78f1-bc06-be503a6c111b", "name": "Juana"})
+    ),
+    responses(
+        (status = 200, description = "The author entry was updated in the DB."),
+        (status = 401, description = "The client has no access to this resource."),
+        (status = 404, description = "An author identified by the given ID didn't exist in the DB."),
     )
 )]
-#[instrument(skip(pool, token))]
+#[instrument(skip(pool, token, path), fields(author_id = %path.0))]
 #[patch("{id}")]
 pub async fn patch_author(
     path: Path<(String,)>,
@@ -53,22 +54,18 @@ pub async fn patch_author(
     pool: Data<MySqlPool>,
     token: Query<AuthData>,
 ) -> Result<HttpResponse, Box<dyn Error>> {
-    info!("Author PATCH request received");
-
-    // // Access control
+    // Access control
     check_access(&pool, &token.api_key).await?;
-    info!("Access granted");
+    debug!("Access granted");
 
-    // Log the received payload
-    debug!("Author entry: {:?}", req);
-    let author_id = path.0.clone();
+    let author_id = &path.0;
 
     // First, get the current entry for the author identified by its ID.
-    let mut existing_author = get_author_from_db(&pool, &author_id).await?;
+    let mut existing_author = get_author_from_db(&pool, author_id).await?;
     existing_author.update_from(&req);
-    info!("Author modified: {:#?}", existing_author);
+    debug!("Author modified: {:#?}", existing_author);
     modify_author_from_db(&pool, &existing_author).await?;
-    info!("Author entry {author_id} patched");
+    info!("Author entry {author_id} modified");
 
-    Ok(HttpResponse::Accepted().finish())
+    Ok(HttpResponse::Ok().finish())
 }
