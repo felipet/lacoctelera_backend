5b142e4b16f27bc193e950ef376a816fadc56259
Felipe Torres González <torresfelipex1@gmail.com>
2024-12-12 14:14:06 +0100

    Post recipe implemented and integration test
    [GH-41]

--- src/routes/recipe/utils.rs
+++ src/routes/recipe/utils.rs
@@ -4,16 +4,131 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use crate::domain::Recipe;
-use sqlx::MySqlPool;
+use crate::domain::{DataDomainError, Recipe, ServerError, Tag};
+use sqlx::{Executor, MySqlPool};
 use std::error::Error;
-use tracing::instrument;
+use tracing::{error, instrument};
 use uuid::Uuid;
 
-#[instrument(skip(_pool))]
+#[instrument(skip(pool))]
 pub async fn register_new_recipe(
-    _pool: &MySqlPool,
-    _recipe: &Recipe,
+    pool: &MySqlPool,
+    recipe: &Recipe,
 ) -> Result<Uuid, Box<dyn Error>> {
-    todo!()
+    // First, let's handle tags. If tags are already defined in the system, add a new entry in the `Tagged` table.
+    // Otherwise, register the new tag, and add the entry in `Tagged`.
+
+    if let Some(tags) = recipe.tags() {
+        for tag in tags {
+            sqlx::query!(
+                "INSERT IGNORE INTO `Tag` SET `identifier` = ?",
+                tag.identifier
+            )
+            .execute(pool)
+            .await
+            .map_err(|e| {
+                error!("{e}");
+                ServerError::DbError
+            })?;
+        }
+    }
+
+    if let Some(tags) = recipe.author_tags() {
+        for tag in tags {
+            sqlx::query!(
+                "INSERT IGNORE INTO `Tag` SET `identifier` = ?",
+                tag.identifier
+            )
+            .execute(pool)
+            .await
+            .map_err(|e| {
+                error!("{e}");
+                ServerError::DbError
+            })?;
+        }
+    }
+
+    let new_id = Uuid::now_v7();
+
+    let mut transaction = pool.begin().await.map_err(|e| {
+        error!("{e}");
+        ServerError::DbError
+    })?;
+
+    let query = sqlx::query!(
+        r#"INSERT INTO `Cocktail` (`id`, `name`, `description`, `category`, `image_id`, `url`, `rating`, `owner`, `steps`)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"#,
+        new_id.to_string(),
+        recipe.name(),
+        recipe.description(),
+        recipe.category().to_string(),
+        recipe.image_id(),
+        recipe.url(),
+        recipe.rating().to_string(),
+        recipe.owner().map(|s| s.to_string()),
+        recipe.steps().join("/n"),
+    );
+
+    transaction.execute(query).await.map_err(|e| {
+        error!("{e}");
+        ServerError::DbError
+    })?;
+
+    for ingredient in recipe.ingredients() {
+        transaction
+            .execute(sqlx::query!(
+                "INSERT INTO `UsedIngredient` (`cocktail_id`, `ingredient_id`, `amount`) VALUES (?, ?, ?)",
+                new_id.to_string(),
+                ingredient.ingredient_id.to_string(),
+                &format!("{} {}", ingredient.quantity, ingredient.unit.to_string()),
+            ))
+            .await
+            .map_err(|e| {
+                error!("{e}");
+                ServerError::DbError
+            })?;
+    }
+
+    if let Some(tags) = recipe.author_tags() {
+        for tag in tags {
+            transaction
+                .execute(sqlx::query!(
+                    "INSERT INTO `Tagged` (`id`, `cocktail_id`, `type`, `tag`) VALUES (?, ?, ?, ?)",
+                    Uuid::now_v7().to_string(),
+                    new_id.to_string(),
+                    "author",
+                    tag.identifier,
+                ))
+                .await
+                .map_err(|e| {
+                    error!("{e}");
+                    ServerError::DbError
+                })?;
+        }
+    }
+
+    if let Some(tags) = recipe.tags() {
+        for tag in tags {
+            transaction
+                .execute(sqlx::query!(
+                    "INSERT INTO `Tagged` (`id`, `cocktail_id`, `type`, `tag`) VALUES (?, ?, ?, ?)",
+                    Uuid::now_v7().to_string(),
+                    new_id.to_string(),
+                    "backend",
+                    tag.identifier,
+                ))
+                .await
+                .map_err(|e| {
+                    error!("{e}");
+                    ServerError::DbError
+                })?;
+        }
+    }
+
+    transaction.commit().await.map_err(|e| {
+        error!("{e}");
+        ServerError::DbError
+    })?;
+
+    Ok(new_id)
 }
--- tests/api/recipe_api.rs
+++ tests/api/recipe_api.rs
@@ -11,11 +11,12 @@ use crate::{
     },
 };
 use actix_web::http::StatusCode;
-use lacoctelera::domain::{QuantityUnit, Recipe, RecipeContains};
+use lacoctelera::domain::{QuantityUnit, Recipe, RecipeContains, Tag};
 use pretty_assertions::assert_eq;
 use reqwest::Response;
-use sqlx::{Executor, MySqlPool};
-use tracing::{debug, error, info};
+use serde::Deserialize;
+use sqlx::MySqlPool;
+use tracing::info;
 use uuid::Uuid;
 
 pub struct RecipeApiTester {
@@ -110,73 +111,243 @@ impl TestObject for RecipeApiTester {
     }
 }
 
-// #[actix_web::test]
-// async fn post_no_credentials() -> Result<(), String> {
-//     info!("Test Case::resource::/recipe (POST) -> Add a new valid recipe entry");
-//     let mut test_builder = RecipeApiBuilder::default();
-//     TestBuilder::author_api_no_credentials(&mut test_builder);
-//     let test = test_builder.build().await;
+#[actix_web::test]
+async fn post_no_credentials() -> Result<(), String> {
+    info!("Test Case::resource::/recipe (POST) -> Add a new valid recipe entry");
+    let mut test_builder = RecipeApiBuilder::default();
+    TestBuilder::author_api_no_credentials(&mut test_builder);
+    let test = test_builder.build().await;
 
-//     let ingredients = [RecipeContains {
-//         quantity: 1.0,
-//         unit: QuantityUnit::Cups,
-//         ingredient_id: Uuid::now_v7(),
-//     }];
+    let seed = true;
+    let fixture = fixtures::FixtureSeeder::new(test.db_pool())
+        .with_ingredients(seed)
+        .with_authors(seed)
+        .seed()
+        .await?;
 
-//     let recipe = Recipe::new(
-//         None,
-//         "Dummy Recipe",
-//         None,
-//         None,
-//         None,
-//         "easy",
-//         None,
-//         None,
-//         &ingredients,
-//         &["Pour everything into a cup and enjoy."],
-//         Some(&Uuid::now_v7().to_string()),
-//     )
-//     .map_err(|e| e.to_string())?;
-//     let response = test.post(&recipe).await;
-//     // This will change once the backend handles properly unauthorised requests.
-//     assert_eq!(response.status().as_u16(), StatusCode::BAD_REQUEST);
+    let ingredients = fixture
+        .ingredient
+        .expect("Failed to extract ingredient fixture")
+        .valid_fixtures;
 
-//     Ok(())
-// }
+    let included_ingredients = &[
+        RecipeContains {
+            quantity: 1.0,
+            unit: QuantityUnit::Ounces,
+            ingredient_id: ingredients[0].id().unwrap(),
+        },
+        RecipeContains {
+            quantity: 30.0,
+            unit: QuantityUnit::MilliLiter,
+            ingredient_id: ingredients[1].id().unwrap(),
+        },
+    ];
 
-// #[actix_web::test]
-// async fn post_with_credentials() -> Result<(), String> {
-//     info!("Test Case::resource::/recipe (POST) -> Add a new valid recipe entry");
-//     let mut test_builder = RecipeApiBuilder::default();
-//     TestBuilder::author_api_with_credentials(&mut test_builder);
-//     let test = test_builder.build().await;
+    let authors = fixture
+        .author
+        .expect("Failed to extract author fixture")
+        .valid_fixtures;
 
-//     let author = valid_author(true, None);
-//     let author_id = seed_author(test.db_pool(), &[author]).await?;
+    let recipe = Recipe::new(
+        None,
+        "Dummy Recipe",
+        None,
+        None,
+        None,
+        "easy",
+        None,
+        None,
+        included_ingredients,
+        &["Pour everything into a cup and enjoy."],
+        Some(&authors[0].id().unwrap().to_string()),
+    )
+    .map_err(|e| e.to_string())?;
+    let response = test.post(&recipe).await;
+    // This will change once the backend handles properly unauthorised requests.
+    assert_eq!(response.status().as_u16(), StatusCode::BAD_REQUEST);
 
-//     let ingredients = [RecipeContains {
-//         quantity: 1.0,
-//         unit: QuantityUnit::Cups,
-//         ingredient_id: Uuid::now_v7(),
-//     }];
+    Ok(())
+}
 
-//     let recipe = Recipe::new(
-//         None,
-//         "Dummy Recipe",
-//         None,
-//         None,
-//         None,
-//         "easy",
-//         None,
-//         None,
-//         &ingredients,
-//         &["Pour everything into a cup and enjoy."],
-//         None,
-//     )
-//     .map_err(|e| e.to_string())?;
-//     let response = test.post(&recipe).await;
-//     assert_eq!(response.status().as_u16(), StatusCode::OK);
-//     assert!(Uuid::parse_str(&response.text().await.expect("Failed to retrieve response")).is_ok());
+#[actix_web::test]
+async fn post_with_credentials() -> Result<(), String> {
+    info!("Test Case::resource::/recipe (POST) -> Add a new valid recipe entry");
+    let mut test_builder = RecipeApiBuilder::default();
+    TestBuilder::author_api_with_credentials(&mut test_builder);
+    let test = test_builder.build().await;
 
-//     Ok(())
-// }
+    let seed = true;
+    let fixture = fixtures::FixtureSeeder::new(test.db_pool())
+        .with_ingredients(seed)
+        .with_authors(seed)
+        .seed()
+        .await
+        .expect("Failed to build a fixture");
+
+    let ingredients = fixture
+        .ingredient
+        .expect("Failed to extract ingredient fixture")
+        .valid_fixtures;
+
+    let included_ingredients = &[
+        RecipeContains {
+            quantity: 1.0,
+            unit: QuantityUnit::Ounces,
+            ingredient_id: ingredients[0].id().unwrap(),
+        },
+        RecipeContains {
+            quantity: 30.0,
+            unit: QuantityUnit::MilliLiter,
+            ingredient_id: ingredients[1].id().unwrap(),
+        },
+    ];
+
+    let authors = fixture
+        .author
+        .expect("Failed to extract author fixture")
+        .valid_fixtures;
+
+    let tags = [
+        Tag {
+            identifier: "cocktail".to_owned(),
+        },
+        Tag {
+            identifier: "delicious".to_owned(),
+        },
+    ];
+
+    let author_tags = [
+        Tag {
+            identifier: "demo".to_owned(),
+        },
+        Tag {
+            identifier: "dummy".to_owned(),
+        },
+    ];
+
+    let recipe = Recipe::new(
+        None,
+        "Dummy Recipe",
+        None,
+        Some(&author_tags),
+        Some(&tags),
+        "easy",
+        Some("A simple description"),
+        None,
+        included_ingredients,
+        &["Pour everything into a cup and enjoy."],
+        Some(&authors[0].id().unwrap().to_string()),
+    )
+    .expect("Failed to build a new recipe");
+    let response = test.post(&recipe).await;
+    assert_eq!(response.status().as_u16(), StatusCode::OK);
+
+    #[derive(Deserialize)]
+    struct Id {
+        pub id: Uuid,
+    }
+
+    let json = response.json::<Id>().await;
+    assert!(json.is_ok());
+    let id = json.unwrap();
+
+    let recipe_from_db = sqlx::query!("SELECT * FROM `Cocktail` WHERE `id`=?", id.id.to_string(),)
+        .fetch_optional(test.db_pool())
+        .await
+        .expect("Failed to retrieve a cocktail entry from the DB");
+
+    let recipe_from_db = match recipe_from_db {
+        Some(recipe) => recipe,
+        None => return Err("Failed to retrieve the inserted recipe from the DB".to_owned()),
+    };
+
+    let ingredients_record = sqlx::query!(
+        "SELECT * FROM `UsedIngredient` WHERE `cocktail_id`=?",
+        id.id.to_string(),
+    )
+    .fetch_all(test.db_pool())
+    .await
+    .expect("Failed to retrieve a record from the DB");
+
+    let mut ingredients = Vec::new();
+
+    for record in ingredients_record {
+        let split: Vec<&str> = record.amount.split(" ").collect();
+        let quantity = split[0]
+            .parse::<f32>()
+            .expect("Failed to parse the quantity of the ingredient");
+        let unit: QuantityUnit = split[1]
+            .try_into()
+            .expect("Failed to parses the quantity unit");
+
+        ingredients.push(RecipeContains {
+            quantity,
+            unit,
+            ingredient_id: Uuid::parse_str(&record.ingredient_id).expect("Failed to parse UUID"),
+        });
+    }
+
+    let tags_from_db = sqlx::query!(
+        "SELECT * FROM `Tagged` WHERE `cocktail_id`=?",
+        id.id.to_string(),
+    )
+    .fetch_all(test.db_pool())
+    .await
+    .map_err(|e| e.to_string())?;
+
+    let author_tags: Vec<Tag> = tags_from_db
+        .iter()
+        .filter(|e| e.r#type == "author")
+        .map(|e| Tag {
+            identifier: e.tag.clone(),
+        })
+        .collect();
+
+    let tags: Vec<Tag> = tags_from_db
+        .iter()
+        .filter(|e| e.r#type == "backend")
+        .map(|e| Tag {
+            identifier: e.tag.clone(),
+        })
+        .collect();
+
+    let received_recipe = Recipe::new(
+        Some(Uuid::parse_str(&recipe_from_db.id).expect("Failed to parse UUID")),
+        &recipe_from_db.name,
+        recipe_from_db.image_id.as_deref(),
+        Some(&author_tags),
+        Some(&tags),
+        &recipe_from_db
+            .category
+            .expect("Failed to extract recipe's category"),
+        recipe_from_db.description.as_deref(),
+        recipe_from_db.url.as_deref(),
+        &ingredients,
+        &stepize(&recipe_from_db.steps),
+        recipe_from_db.owner.as_deref(),
+    )
+    .expect("Failed to build a new recipe");
+
+    assert_eq!(recipe.name(), received_recipe.name());
+    assert_eq!(recipe.image_id(), received_recipe.image_id());
+    assert_eq!(recipe.category(), received_recipe.category());
+    assert_eq!(recipe.description(), received_recipe.description());
+    assert_eq!(recipe.url(), received_recipe.url());
+    assert_eq!(recipe.ingredients(), received_recipe.ingredients());
+    assert_eq!(recipe.steps(), received_recipe.steps());
+    assert_eq!(recipe.owner(), received_recipe.owner());
+    assert_eq!(recipe.tags(), received_recipe.tags());
+    assert_eq!(recipe.author_tags(), received_recipe.author_tags());
+
+    Ok(())
+}
+
+fn stepize<'a>(steps: &'a str) -> Vec<&'a str> {
+    let mut step_list = Vec::new();
+
+    for line in steps.split("/n") {
+        step_list.push(line);
+    }
+
+    step_list
+}
